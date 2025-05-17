f0e58df7ba003d9035775a87f605e92abef898e3
Animesh Singh <animeshrathore255@gmail.com>
2025-05-17 13:31:45 +0530

    added dutch algo

---
+++ dutch-national-algo.cpp
@@ -0,0 +1,12 @@
+// while (mid <= high) {
+//     if (nums[mid] == 0) {
+//         swap(nums[low], nums[mid]);
+//         low++;
+//         mid++;
+//     } else if (nums[mid] == 1) {
+//         mid++;
+//     } else { // nums[mid] == 2
+//         swap(nums[mid], nums[high]);
+//         high--;
+//     }
+// }
